7c6e8ea5efe8dbcf66f43c8878a4ef458b761da6
statman95 <shortcommapeter@gmail.com>
2020-09-30 16:22:00 -0500

    Changing for Test Pipeline

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("AWS Continuous Integration Tutorial"));
+app.get("/", (req, res) =>
+	res.send("AWS Continuous Integration Tutorial- New Change")
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
